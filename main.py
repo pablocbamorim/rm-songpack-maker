0ebc06c832e1ee26126db08cbe3e7e85deb6773e
Pablo <amorimpa06@gmail.com>
2026-09-14 20:22:02 -0300

    Make imports discoverable by PyInstaller

--- main.py
+++ main.py
@@ -2,18 +2,19 @@
 """Entry point for the ReactiveMusic Songpack Editor."""
 import sys
 
+try:
+    import app
+    import ui_enhancements
+except ImportError as exc:
+    if "tkinter" in str(exc).lower():
+        print("This program needs Python's 'tkinter' GUI library.",
+              file=sys.stderr)
+        sys.exit(1)
+    raise
+
 
 def main():
-    try:
-        import app
-    except ImportError as exc:
-        if "tkinter" in str(exc).lower():
-            print("This program needs Python's 'tkinter' GUI library.",
-                  file=sys.stderr)
-            sys.exit(1)
-        raise
     application = app.App()
-    import ui_enhancements
     ui_enhancements.install(application)
     application.mainloop()
 
