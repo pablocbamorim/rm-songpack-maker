70e5eefdfe6c29259820425606277f6e13745429
Pablo <amorimpa06@gmail.com>
2026-09-15 18:07:25 -0300

    Bump UI build version to v0.1.7.2

--- version_ui.py
+++ version_ui.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import webbrowser
 
-BUILD_VERSION = "v0.1.7.1"
+BUILD_VERSION = "v0.1.7.2"
 REPOSITORY_URL = "https://github.com/pablocbamorim/rm-songpack-maker"
 README_URL = f"{REPOSITORY_URL}#readme"
 
