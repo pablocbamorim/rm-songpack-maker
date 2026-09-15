049259d9377a0fa995c1bfd0d90a8bb87aa6ea56
Pablo <amorimpa06@gmail.com>
2026-09-15 18:35:59 -0300

    Bump patch version for priority fix

--- version_ui.py
+++ version_ui.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import webbrowser
 
-BUILD_VERSION = "v0.1.7.3"
+BUILD_VERSION = "v0.1.7.4"
 REPOSITORY_URL = "https://github.com/pablocbamorim/rm-songpack-maker"
 README_URL = f"{REPOSITORY_URL}#readme"
 
