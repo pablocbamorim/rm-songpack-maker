9b714e1aa6a6af7a8e5ff10a42a32c3b8b775a34
Pablo <amorimpa06@gmail.com>
2026-09-15 00:50:10 -0300

    Bump version to 0.1.5.0 for release distribution feature

--- version_ui.py
+++ version_ui.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import webbrowser
 
-BUILD_VERSION = "v0.1.4.3"
+BUILD_VERSION = "v0.1.5.0"
 REPOSITORY_URL = "https://github.com/pablocbamorim/rm-songpack-maker"
 README_URL = f"{REPOSITORY_URL}#readme"
 
