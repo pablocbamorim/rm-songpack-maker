c165c0f83dd6636f9db8f9c9f56248291d744140
Pablo <amorimpa06@gmail.com>
2026-09-15 00:39:35 -0300

    chore: bump editor version to 0.1.4.3

--- version_ui.py
+++ version_ui.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import webbrowser
 
-BUILD_VERSION = "v0.1.4.2"
+BUILD_VERSION = "v0.1.4.3"
 REPOSITORY_URL = "https://github.com/pablocbamorim/rm-songpack-maker"
 README_URL = f"{REPOSITORY_URL}#readme"
 
